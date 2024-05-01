5ab6a0fe1a0c053d4f9680490e7511e9136c1448
Johann Osnaya <johann.osnaya@gmail.com>
2024-04-30 21:08:08 -0600

    Fix directory composition for vercel deployment

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (_req, res) => {
     console.log('someone pinged here');
-    res.send('pong');
+    res.send('<h1>Pong</h1>');
 });
 
 export default router;
